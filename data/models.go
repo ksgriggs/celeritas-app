76e1ab58bdbfb043fbcbf5b359b9049ebfaeb642
Scott Griggs <sgriggs@gmail.com>
2025-09-02 19:09:21 -0400

    Initial commit

---
+++ data/models.go
@@ -0,0 +1,45 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	db2 "github.com/upper/db/v4"
+	"github.com/upper/db/v4/adapter/mysql"
+	"github.com/upper/db/v4/adapter/postgresql"
+)
+
+var db *sql.DB
+var upper db2.Session
+
+type Models struct {
+	// any models inserted here and in the New function
+	// are easily accessible throughout the application
+}
+
+// New initializes and returns a new Models instance for interacting with the application's database abstractions.
+func New(databasePool *sql.DB) Models {
+	db = databasePool
+
+	switch os.Getenv("DATABASE_TYPE") {
+	case "mysql", "mariadb":
+		upper, _ = mysql.New(databasePool)
+	case "postgres", "postgresql":
+		upper, _ = postgresql.New(databasePool)
+	default:
+		// do nothing
+	}
+
+	return Models{}
+}
+
+// getInsertID converts the insert result ID into an int type, handling different underlying types such as int64 and int.
+func getInsertID(i db2.ID) int {
+	idType := fmt.Sprintf("%T", i)
+	if idType == "int64" {
+		return int(i.(int64))
+	}
+
+	return i.(int)
+}
